8945acf9d70652c2589eaba9a309d46c0c6af472
Aidas Klimas <aidaskk@gmail.com>
2013-08-23 17:09:50 +0300

    feat: basic sample updated

--- samples/basic/app.js
+++ samples/basic/app.js
@@ -3,17 +3,23 @@
 
 /**
  * The main app module
+ * @name app
  * @type {angular.Module}
  */
 var app = angular.module('app', ['ngResumable'])
 .config(['resumableFactoryProvider', function (resumableFactoryProvider) {
-    resumableFactoryProvider.defaults = {
-        target: '/upload',
-        permanentErrors:[404, 500, 501],
-        minFileSize: 0
-    };
-    // Can be used with different implementations of Resumable.js
-    // resumableFactoryProvider.factory = MaybeResumable;
+  resumableFactoryProvider.defaults = {
+    target: 'upload.php',
+    permanentErrors: [404, 500, 501],
+    maxChunkRetries: 1,
+    chunkRetryInterval: 5000,
+    simultaneousUploads: 4
+  };
+  resumableFactoryProvider.on('catchAll', function (event) {
+    console.log('catchAll', arguments);
+  });
+  // Can be used with different implementations of Resumable.js
+  // resumableFactoryProvider.factory = MaybeResumable;
 }]);
 
 app.controller('MainCtrl', function ($scope) {
