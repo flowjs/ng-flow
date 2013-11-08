4f8ed0f57ffc0b09ca9fa00d0bb5aff64011d1d9
Aidas Klimas <aidaskk@gmail.com>
2013-11-08 15:46:46 +0200

    feat: grunt file build command

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= pkg.version %> */\n'
+      },
+      build: {
+        src: 'build/ng-flow.js',
+        dest: 'build/ng-flow.min.js'
+      }
+    },
+    concat: {
+      flow: {
+        files: {
+          'build/ng-flow.js': [
+            'bower_components/flow.js/src/flow.js',
+            'src/provider.js',
+            'src/directives/init.js',
+            'src/directives/btn.js',
+            'src/directives/drop.js',
+            'src/directives/img.js',
+            'src/directives/transfers.js',
+            'src/ng-flow.js',
+          ]
+        }
+      }
+    }
+  });
+
+  // Loading dependencies
+  for (var key in grunt.file.readJSON("package.json").devDependencies) {
+    if (key !== "grunt" && key.indexOf("grunt") === 0) grunt.loadNpmTasks(key);
+  }
+
+  grunt.registerTask('build', ['concat', 'uglify:build']);
+
+};
