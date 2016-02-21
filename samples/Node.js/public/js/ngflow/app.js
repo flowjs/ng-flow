e70698f6815b47ec2d6e162044d3ec568350163f
Quentin <github@quantumhpc.com>
2016-02-21 17:18:51 -0500

    Nodejs sample with angularjs
    Fork between the sample in flow.js/samples/Node.js/ and ng-flow/samples/basic/ with a separated router module and a backend in javascript with flow-node.js
    
    I do not claim ownership on this piece of code but though it could be useful to have a complete example in node.js

---
+++ samples/Node.js/public/js/ngflow/app.js
@@ -0,0 +1,24 @@
+/*global angular */
+'use strict';
+
+/**
+ * The main app module
+ * @name app
+ * @type {angular.Module}
+ */
+var app = angular.module('app', ['flow'])
+.config(['flowFactoryProvider', function (flowFactoryProvider) {
+  flowFactoryProvider.defaults = {
+    target: '/upload',
+    permanentErrors: [404, 500, 501],
+    testChunks: false,
+    maxChunkRetries: 1,
+    chunkRetryInterval: 5000,
+    simultaneousUploads: 4
+  };
+  flowFactoryProvider.on('catchAll', function (event) {
+    console.log('catchAll', arguments);
+  });
+  // Can be used with different implementations of Flow.js
+  // flowFactoryProvider.factory = fustyFlowFactory;
+}]);
